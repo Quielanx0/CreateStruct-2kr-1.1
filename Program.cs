2ffd13fb7e8efd7dbee5dbfb46bb6487893f5843
Quiel <Quiel@QUIELANX0-MSI>
2021-09-09 09:53:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CreateStruct
+{
+    struct Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Person(string _firstName, string _lastName, int _age);
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
